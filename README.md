892051c647ca3744eabf84338c33d3ee3088a2bd
Finlay Miller <finlay@dal.ca>
2018-06-13 22:09:12 -0300

    Improved README.md

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-fsh
+## fsh: A simple C shell
+### CSCI3120 Assignment 1
+
+Finlay Miller - B00675696 - 13 JUNE 2018
+
+---
+To compile: `gcc -g -o fsh fsh.c externs.c`
+
+The included image _fsh.png_ shows several types of command being executed:
+1. `!2` - Executing the 2nd last command executed
+2. `mkdir tempfolder &` - Creation of a child process
+3. `history` - Printing of the last 10 command executed
--- fsh.c
+++ fsh.c
@@ -1,104 +1,92 @@
+/*
+    FILE:       fsh.c
+    PURPOSE:    mainfile for program
+    AUTHOR:     Finlay Miller B00675696
+    DATE:       13 June 2018
+*/
+
 #include "fsh.h"
 
 int main(void)
 {
-    char *args[MAX_LINE_LEN / 2 + 1];   // an array of arguments
-    char command[MAX_LINE_LEN];         // the input command
-    char *history[MAX_LINE_LEN / 2 + 1];// an array of saved commands
-    char *current_cmd;                  // the command currently being worked with
-    char *tmp_command;                  // a temporary command holder used when handling '!!' or '!'
-    char *tkn;                          // a pointer to the first token of the command
-    int should_run = 1;                 // flag for controlling whether or not the program should run
-    int i;                              // the counter for the args array
-    int j;                              // the counter used to initially clear args array
-    int amperflag;                      // flag set when '&' character is detected
-    int count = 0;                      // the number of commands in history
-    pid_t pid;                          // the process identification number
-    size_t size;                        // the size of the history array
-    int k = 0;
+    /*
+        PURPOSE:    Creates a simple C shell
+        INPUT:      None
+        OUTPUT:     0 if completion successful, 1 otherwise
+    */
 
-    //lsta
-    //lstb
-    //con-ptr-500
-    //rcl
+    // variable declaration
+    char *args[MAX_LINE_LEN / 2 + 1];    // an array of arguments
+    char command[MAX_LINE_LEN];          // the input command
+    char *history[MAX_LINE_LEN / 2 + 1]; // an array of saved commands
+    char *current_cmd;                   // the command currently being worked with
+    char *tmp_cmd;                       // a temporary command holder used when handling '!!' or '!'
+    char *tkn;                           // a pointer to the first token of the command
+    int should_run = 1;                  // flag for controlling whether or not the program should run
+    int i;                               // the counter for the args array
+    int j;                               // the counter used to initially clear args array
+    int amperflag;                       // flag set when '&' character is detected
+    int count = 0;                       // the number of commands in history
+    int index;                           // the index of the command to execute, used with '!'
+    pid_t pid;                           // the process identification number
+    size_t size;                         // the size of the history array
+
+    // initial screen writes
     printf("\n\nfsh shell interface - Version %s (%s)\n", CURRENT_VERSION, UPDATE_DATE);
-	printf("Written by Finlay Miller (B00675696) for CSCI3120 Assignment #1\n");
+    printf("Written by Finlay Miller (B00675696) for CSCI3120 Assignment #1\n");
     printf("Enter 'fhelp' for help.\n\n\n");
 
-    while (should_run)
+    while (should_run) // should_run = 1
     {
         // initialize values
-        i = 0;
-        amperflag = 0;
         fflush(stdin);
         fflush(stdout);
+        i = 0;
         command[0] = '\0';
+        amperflag = 0;
         memset(command, 0, MAX_LINE_LEN);
         for (j = 0; j < (MAX_LINE_LEN / 2 + 1); j++)
-        {
             args[j] = '\0';
-            history[j] = NULL;
-        } 
 
-        printf("fsh> ");
+        // print prompt
+        printf("fsh>");
 
-        // get input and check for exit command
+        // get input
         fgets(command, MAX_LINE_LEN, stdin);
         strtok(command, "\n");
+
+        // check right away for exit command
         if (strcmp(command, "exit") == 0)
         {
             should_run = 0;
             break;
         }
 
-        // set command pointer to preserve original string
+        // copy command pointer to preserve original command
         current_cmd = command;
 
-        // check for 'fhelp' command
-        if (strcmp(current_cmd, "fhelp") == 0)
-        {
-            if(!print_help())
-                printf("Failed to print help.\n");
-        }
-
-        if ((strcmp(current_cmd, "history") != 0) && (current_cmd[0] != '!'))
-        {
-            history[count++] = strdup(current_cmd);
-        }
         // check for 'history' command
-        else if (strcmp(current_cmd, "history") == 0)
+        if (strcmp(current_cmd, "history") == 0)
         {
             if (count == 0)
             {
                 printf("No commands in history\n");
                 continue;
             }
-            
-            k = 0;
-            while(history[k] != NULL) 
-            {
-                printf("history[k] is %s\n", history[k]);
-                k++;
-            }
-            size = k;
-
-            print_history(history, count, size);
+            print_history(history, count);
             continue;
         }
-
         // check for '!!' command
-        if ((strcmp(current_cmd, "!!") == 0))
+        else if ((strcmp(current_cmd, "!!") == 0))
         {
             if (count == 0)
             {
                 printf("No commands in history\n");
                 continue;
             }
-            
-            // run last command
-            tmp_command = strdup(run_last_cmd(history, count));
-            history[count++] = strdup(tmp_command);
-            strcpy(command, tmp_command);
+            tmp_cmd = strdup(last_cmd(history, count));
+            history[count++] = strdup(tmp_cmd);
+            strcpy(command, tmp_cmd);
         }
         // check for '!' command
         else if (current_cmd[0] == '!')
@@ -112,15 +100,24 @@ int main(void)
                 printf("No command in history\n");
                 continue;
             }
-            
-            // run specified command
-            tmp_command = strdup(run_specific_cmd(history, count, index));
-            history[count++] = strdup(tmp_command);
-            strcpy(command, tmp_command);
+            // execute selected command
+            tmp_cmd = strdup(former_cmd(history, count, index));
+            history[count++] = strdup(tmp_cmd);
+            strcpy(command, tmp_cmd);
         }
-
-        if(strcmp(current_cmd, "joke") == 0)
+        // check for 'fhelp' command
+        else if (strcmp(current_cmd, "fhelp") == 0)
+        {
+            print_help();
+        }
+        // check for 'joke' command
+        else if (strcmp(current_cmd, "joke") == 0)
+        {
             joke();
+        }
+        // default
+        else
+            history[count++] = strdup(current_cmd);
 
         // once specific commands are checked for, tokenize the command string
         tkn = strtok(command, " ");
@@ -129,59 +126,55 @@ int main(void)
             args[i++] = tkn;
             tkn = strtok(NULL, " ");
         }
-
-        // add null terminator
         args[i] = NULL;
 
-        // check for ampersand
+        // check for '&'
         if ((strcmp(args[i - 1], "&") == 0))
         {
             amperflag = 1;
             args[i - 1] = NULL;
         }
 
-        // check for fork
+        // once tokenized, fork
         pid = fork();
 
         if (pid < 0)
         {
-            fprintf(stderr, "Fork failed.\n");
+            fprintf(stderr, "Fork failed");
             return 1;
         }
         else if (pid == 0)
         {
-            // child
+            // make child
             if (strcmp(current_cmd, "exit") == 0)
+            {
+                should_run = 0;
                 break;
+            }
 
-            if (strcmp(current_cmd, "history") == 0 || strcmp(current_cmd, "!!") == 0 || (current_cmd[0] == '!'))
-            {
-                printf("/n/n/t/tHERE/n/n");
-                while(history[k] != '\0') k++;
-                size = k;
-                print_history(history, count, size);
-            }
+            // add command to history
             else
-            {
-                // add command to history
                 execvp(args[0], args);
-            }
         }
         else
         {
-            // parent
+            // make parent
             if (strcmp(current_cmd, "exit") == 0)
+            {
+                should_run = 0;
                 break;
+            }
 
             if (amperflag)
             {
-                // sleep until seconds have elapsed
+                // sleep until 1 second has elapsed
                 sleep(1);
-                printf("\tDone.\n");
+                printf("\nDone\n");
             }
-            // wait for child processes to complete
+            // wait until child is complete
             else wait(NULL);
         }
-    }
+    } // should_run != 1
+
     return 0;
 }
